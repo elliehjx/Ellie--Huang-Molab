e6a03996a9100d7abbae468474823cd7806d1583
Jiaxin(Ellie) Huang <jh6003@nyu.edu>
2022-02-04 16:33:13 -0500

    New Changes

---
+++ MOBILE-APP-COURSE-PRACTICE.playground/Pages/WEEK2-INCLASS01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+/*:
+ # Welcome to Playgrounds
+ */
+
+import UIKit
+
+var greeting = "Hello, playground"
+
+print(greeting)
+
+var m = 10
+
+//for loop
+
+for h in 1...20 {
+  let v = h * m
+  print("m \(m) h \(h) v \(v) ")
+  m -= 1
+}
+
