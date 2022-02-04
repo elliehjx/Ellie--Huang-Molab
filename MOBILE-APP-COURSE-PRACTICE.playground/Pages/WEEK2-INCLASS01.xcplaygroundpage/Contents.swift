30bf70b10fbbd8426832825dcd99fd5967695aa1
Jiaxin(Ellie) Huang <jh6003@nyu.edu>
2022-02-04 17:49:25 -0500

    new changes

--- MOBILE-APP-COURSE-PRACTICE.playground/Pages/WEEK2-INCLASS01.xcplaygroundpage/Contents.swift
+++ MOBILE-APP-COURSE-PRACTICE.playground/Pages/WEEK2-INCLASS01.xcplaygroundpage/Contents.swift
@@ -1,20 +1,43 @@
-/*:
- # Welcome to Playgrounds
- */
 
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let str = "🌻🌷🌼🌺🌸"
 
-print(greeting)
-
-var m = 10
-
-//for loop
-
-for h in 1...20 {
-  let v = h * m
-  print("m \(m) h \(h) v \(v) ")
-  m -= 1
+func charAt(_ str:String, _ offset:Int) -> String {
+    //variable, starting index and offset
+    //offset means which character we want to extract
+  let index = str.index(str.startIndex, offsetBy: offset)
+  let char = str[index]
+  return String(char)
 }
+
+//get random number from the string
+let randomInt = Int.random(in: 0..<str.count)
+
+var serial = 0
+
+func generateLine (_ n:Int) {
+    var nstr = ""
+    for _ in 0..<n {
+        
+        //question for offset time
+      let offset = (serial % str.count)
+      serial += 1
+      nstr += charAt(str, offset)
+    }
+    print(nstr)
+}
+
+//
+func generateBlock(_ n: Int) {
+  for _ in 0..<n {
+    generateLine(n)
+  }
+}
+
+generateBlock(3)
+print("")
+generateBlock(2)
+print("")
+generateBlock(1)
 
